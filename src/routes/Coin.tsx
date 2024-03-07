3d36fa7c1b0cf0e2c1a9f7c1c7ced6430d642c8f
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-03-07 18:09:41 +0100

    Coin, fetching general & price data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
 
@@ -34,13 +34,27 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async() => {
+      const infoData = await ( 
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, [])
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : (null)}
+      {loading ? <Loader>Loading...</Loader> : <span>Explain types of info, priceInfo to TS</span>}
     </Container>
   );
 }
