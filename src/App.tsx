00a671ecff917f0d4808e7709a3523c08a780921
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-03-14 23:43:33 +0100

    useTransform - rotateZ, background, scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion"
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion"
 import { useEffect } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   display: flex;
-  height: 100vh;
+  height: 200vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
@@ -27,12 +27,21 @@ const boxVars = {
 
 function App() {
   const x = useMotionValue(0); 
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
-  
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x, 
+    [-800, 800], 
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const {scrollYProgress} = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
+    <Wrapper style={{background: gradient}}>
       <Box 
-        style={{ x, scale }}
+        style={{ x, rotateZ, scale }}
         drag="x"
         dragSnapToOrigin
       />
