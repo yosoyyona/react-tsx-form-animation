bf24452ed865dfcb0fa8dcf4041b4b3e1ded8d4c
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-03-16 01:48:47 +0100

    svg, path

--- src/index.tsx
+++ src/index.tsx
@@ -61,7 +61,7 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Source Sans Pro', sans-serif;
     color:black;
     line-height: 1.2;
-    /* background:linear-gradient(135deg,#e09,#d0e); */
+    background:linear-gradient(135deg,#e09,#d0e);
   }
   a {
     text-decoration:none;
