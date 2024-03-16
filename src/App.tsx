bf4aa6569b779297e8595bb4aae49aebac0e344f
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-03-16 21:04:14 +0100

    animation - layout, layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,43 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   display: flex;
   height: 100vh;
   width: 100vw;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVars = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.5 }
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    rotateX: 180,
-    transition: { duration: 0.5 }
-  })
-};
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible(prev => prev === 10 ? 10: prev + 1)
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible(prev => prev === 1 ? 1: prev - 1)
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence custom={back} mode="wait">
-        <Box 
-          custom={back}
-          variants={boxVars} 
-          initial="entry" 
-          animate="center" 
-          exit="exit" 
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? <Circle layoutId="circle" style={{borderRadius: 50}} /> : null}
+      </Box>
+      <Box>
+        {clicked ? <Circle layoutId="circle" style={{borderRadius: 0, scale: 2}} /> : null}
+      </Box>
     </Wrapper>
   );
 };
